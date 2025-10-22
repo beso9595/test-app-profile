1b0cf23ddf73c6a3d47836d88905fb8a82edde10
Beso Bantsadze <beso.bantsadze@gmail.com>
2025-10-22 22:10:10 +0300

    added card component & completed about page

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from "../../shared/components/card/card.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
